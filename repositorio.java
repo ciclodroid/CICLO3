e944bc0c29d5f6bdae15e482a2a9107958f140d9
sintrauac <sintrauac2019@gmail.com>
2022-08-31 16:45:35 -0500

    modificacion 31 agosto 2022 4:45

--- repositorio.java
+++ repositorio.java
@@ -12,3 +12,5 @@ modificacion hecha por el computador negro
 
 
 modificacion hecha por el computador import ja
+
+modificacion hecha por la nueva cuenta en el repositorio que sera usado para el proyecto es aqui.
